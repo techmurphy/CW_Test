4b6ce1bd88b6412873324b052af882a8941fe956
Gaurav Dhawan <gaurav.d6@gmail.com>
2017-06-13 12:41:43 +0200

    Create index.js

--- ThanksBot/index.js
+++ ThanksBot/index.js
@@ -33,7 +33,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-
+/*
 // List out all the thanks recorded in the database
 app.get('/', function (request, response) {
 	pg.connect(DATABASE_URL, function(err, client, done) {
@@ -201,4 +201,4 @@ function verifyRequestSignature(req, res, buf) {
 			throw new Error("Couldn't validate the request signature.");
 		}
 	}
-}
+}*/
